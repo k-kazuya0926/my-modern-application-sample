f581979b0d1d897cca0f792b978f0eb6fad1c670
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-06-22 19:18:49 +0900

    GitHub Actions用のIAMロールとポリシーを追加し、ECRおよびLambdaの権限を定義

--- infra/iam.tf
+++ infra/iam.tf
@@ -48,3 +48,86 @@ data "aws_iam_policy_document" "my-modern-application-sample-tmp-role" {
     resources = ["arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/my-modern-application-sample-tmp:*"]
   }
 }
+
+# TODO: OIDC
+
+# github_actions_role
+resource "aws_iam_role" "github_actions" {
+  name               = "my-modern-application-sample-github-actions-role"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:k-kazuya0926/my-modern-application-sample:*"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "github_actions" {
+  name   = "my-modern-application-sample-github-actions-policy"
+  policy = data.aws_iam_policy_document.github_actions.json
+}
+
+data "aws_iam_policy_document" "github_actions" {
+  statement {
+    sid    = "ECRPermissions"
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ECRRepositoryPermissions"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart"
+    ]
+    resources = [
+      "arn:aws:ecr:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:repository/my-modern-application-sample-hello-world",
+      "arn:aws:ecr:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:repository/my-modern-application-sample-tmp"
+    ]
+  }
+
+  statement {
+    sid    = "LambdaDeployPermissions"
+    effect = "Allow"
+    actions = [
+      "lambda:UpdateFunctionCode",
+      "lambda:GetFunction",
+      "lambda:GetFunctionConfiguration",
+      "lambda:PublishVersion",
+      "lambda:UpdateAlias"
+    ]
+    resources = [
+      "arn:aws:lambda:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:function:my-modern-application-sample-hello-world",
+      "arn:aws:lambda:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:function:my-modern-application-sample-hello-world:*",
+      "arn:aws:lambda:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:function:my-modern-application-sample-tmp",
+      "arn:aws:lambda:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:function:my-modern-application-sample-tmp:*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
+}
