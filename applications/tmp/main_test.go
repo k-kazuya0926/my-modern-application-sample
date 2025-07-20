22db18862c7e836c3fab9711e2092b4a3b161efb
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-07-21 07:39:48 +0900

    tmpにテストを追加

---
+++ applications/tmp/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	ctx := context.Background()
+
+	result, err := handler(ctx)
+	if err != nil {
+		t.Errorf("handler() returned an error: %v", err)
+	}
+
+	if result != "tmp" {
+		t.Errorf("handler() returned %q, expected %q", result, "tmp")
+	}
+}
